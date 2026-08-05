20647fe838ffd21315659484ef38e3dcdab2bbd8
luizfelipense <m.luiz24@escola.pr.gov.br>
2026-08-05 10:49:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let nome;
+let numero1;
+let numero2;
+let resultado1;
+let resultado2;
+let resultado3;
+let resultado4;
+
+nome = prompt("Digite o seu nome");
+numero1 = prompt('Digite um numero:')
+numero2 = prompt("Digite um numero novamente")
+
+
+alert("Olá, " + nome + "! Seja bem vindo a nossa calculadora ");
+
+resultado1 = numero1 + numero2;
+resultado2 = numero1 - numero2;
+resultado3 = numero1 * numero2;
+resultado4 = numero1 / numero2;
+alert("o resultado será: Soma:" + resultado1);
