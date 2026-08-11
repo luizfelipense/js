1257af686edf43b685c3ec29e0d46ff85854b912
luizfelipense <m.luiz24@escola.pr.gov.br>
2026-08-11 08:43:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ let resultado2;
 let resultado3;
 let resultado4;
 
-nome = prompt("Digite o seu nome:");
+function calculadora() {
 
-numero1 = Number(prompt("Digite um número:"));
-numero2 = Number(prompt("Digite outro número:"));
+    nome = prompt("Digite o seu nome:");
 
-alert("Olá, " + nome + "! Seja bem-vindo à nossa calculadora!");
+    numero1 = Number(prompt("Digite um número:"));
+    numero2 = Number(prompt("Digite outro número:"));
 
-resultado1 = numero1 + numero2;
-resultado2 = numero1 - numero2;
-resultado3 = numero1 * numero2;
-resultado4 = numero1 / numero2;
+    alert("Olá, " + nome + "! Seja bem-vindo à nossa calculadora!");
 
-alert("Resultados para " + nome + ":\n\n" +"Soma (+): " + resultado1 + "\n" +"Subtração (-): " + resultado2 + "\n" + "multiplicação (*): " + resultado3 + "/n" + "Divisão (/): " + resultado4);
+    resultado1 = numero1 + numero2;
+    resultado2 = numero1 - numero2;
+    resultado3 = numero1 * numero2;
+    resultado4 = numero1 / numero2;
+
+    alert("Resultados para " + nome + ":\n\n" +"Soma (+): " + resultado1 + "\n" +"Subtração (-): " + resultado2 + "\n" + "multiplicação (*): " + resultado3 + "\n"  + "Divisão (/): " + resultado4);
+}
